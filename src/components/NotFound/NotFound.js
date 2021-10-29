dff9549ecc63d59503d4e4ecbd49b2dcac8efee2
rubelsarker0 <rubelsarker0@outlook.com>
2021-10-29 19:30:16 +0100

    aadded-not-found-page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,7 +1,29 @@
 import React from 'react';
-
+import { Container, Button } from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+import notFoundImg from '../../assets/images/404.png';
 const NotFound = () => {
-	return <div> Page not found</div>;
+	const history = useHistory();
+	const handleGoBack = () => {
+		history.goBack();
+	};
+	return (
+		<Container>
+			<div className="text-center py-5 mt-4">
+				<div>
+					<img className="w-50" src={notFoundImg} alt="" />
+				</div>
+				<div className="pt-5">
+					<Button
+						onClick={handleGoBack}
+						className="btn-login rounded-pill px-5 fw-bold 5 mt-3"
+					>
+						Go back
+					</Button>
+				</div>
+			</div>
+		</Container>
+	);
 };
 
 export default NotFound;
